53759ba44d15ce1f59bc795938afc953a311b408
Pasha Ditkovskyi <pashkadit@gmail.com>
2026-02-13 14:33:14 +0200

    Make ScriptExecutionRepository extend JpaRepository

--- src/main/java/io/github/deschna/scriptmanager/repository/ScriptExecutionRepository.java
+++ src/main/java/io/github/deschna/scriptmanager/repository/ScriptExecutionRepository.java
@@ -1,12 +1,9 @@
 package io.github.deschna.scriptmanager.repository;
 
 import io.github.deschna.scriptmanager.domain.execution.ScriptExecution;
-import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScriptExecutionRepository {
+public interface ScriptExecutionRepository extends JpaRepository<ScriptExecution, UUID> {
 
-    ScriptExecution save(ScriptExecution execution);
-
-    Optional<ScriptExecution> findById(UUID id);
 }
